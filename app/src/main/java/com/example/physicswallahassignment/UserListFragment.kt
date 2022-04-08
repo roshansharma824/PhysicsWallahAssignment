308ec82ae3b8346ddb668d0a0f0f6f911883d161
roshansharma824 <roshansharma824@gmail.com>
2022-04-08 10:59:12 +0530

    Dark mode

--- app/src/main/java/com/example/physicswallahassignment/UserListFragment.kt
+++ app/src/main/java/com/example/physicswallahassignment/UserListFragment.kt
@@ -1,13 +1,13 @@
 package com.example.physicswallahassignment
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.lifecycle.Observer
+
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.custom_action_bar.*
@@ -16,11 +16,8 @@ import kotlinx.android.synthetic.main.fragment_user_list.*
 
 class UserListFragment : Fragment() {
 
-    lateinit var recyclerAdapter: UserListAdapter
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var recyclerAdapter: UserListAdapter
 
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,19 +43,23 @@ class UserListFragment : Fragment() {
 
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     private fun initViewModel() {
-        val viewModel:MainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-        viewModel.getLiveDataObserver().observe(viewLifecycleOwner, Observer {
-            if(it != null) {
-                progressBar.visibility = View.GONE
-                recyclerAdapter.setUserList(it)
-                recyclerAdapter.notifyDataSetChanged()
-            } else {
-                progressBar.visibility = View.GONE
-                Toast.makeText(requireContext(), "Error in getting list", Toast.LENGTH_SHORT).show()
+        val viewModel:MainActivityViewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        with(viewModel) {
+            this.getLiveDataObserver().observe(viewLifecycleOwner) {
+                if (it != null) {
+                    progressBar.visibility = View.GONE
+                    recyclerAdapter.setUserList(it)
+                    recyclerAdapter.notifyDataSetChanged()
+                } else {
+                    progressBar.visibility = View.GONE
+                    Toast.makeText(requireContext(), "Error in getting list", Toast.LENGTH_SHORT)
+                        .show()
+                }
             }
-        })
-        viewModel.makeAPICall()
+            makeAPICall()
+        }
     }
 
 
